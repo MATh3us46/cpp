0b91c31fe36dc61ecb3b160cec6450ea6a607c3a
MATh3us46 <matheussavegnago735@gmail.com>
2025-10-31 17:57:37 -0300

    main

---
+++ 1924.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string curso;
+
+    for(int i=0; i<n; i++){
+        cin >> curso;
+    }
+
+    cout << "Ciencia da Computacao" << endl;
+
+    return 0;
+}
