d4fe49cb3af564cba2d7cfffe176f63ab7e10121
MATh3us46 <matheussavegnago735@gmail.com>
2026-08-22 17:15:06 -0300

    main

---
+++ 2752.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char v[50] = "AMO FAZER EXERCICIO NO URI";
+
+    printf("<%s>\n", v);
+    printf("<%30s>\n", v);
+    printf("<%.20s>\n", v);
+    printf("<%-20s>\n", v);
+    printf("<%-30s>\n", v);
+    printf("<%.30s>\n", v);
+    printf("<%30.20s>\n", v);
+    printf("<%-30.20s>\n", v);
+}
---
+++ 3055.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, m, b;
+
+    cin >> a;
+    cin >> m;
+
+    b = (m - (a / 2.0)) * 2;
+    cout << b << "\n";
+}
